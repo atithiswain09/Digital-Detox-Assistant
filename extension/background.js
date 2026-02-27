c0a4ce4ac63585e4d177a939effe0392da0cbe78
Atithi Swain <atithiswain371@gmail.com>
2026-02-27 20:14:05 +0530

    Feature/extension background (#3)
    * Refactor code structure for improved readability and maintainability
    
    * refactor: clean up background.js by removing unused functions and comments
    
    ---------
    
    Co-authored-by: Sumith Kumar Saini <140690202+Sumith-Kumar-Saini@users.noreply.github.com>

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,6 @@
-// ================= CONFIG =================
+let currentSite = null;
+let startTime = null;
+
 const DEFAULT_BLOCKED = [
   "x.com",
   "facebook.com",
@@ -7,38 +9,53 @@ const DEFAULT_BLOCKED = [
   "youtube.com",
 ];
 
-const RULESET_ID_BASE = 1000;
-
-// ================= HELPERS =================
-/**
- * @param {number} id
- * @param {string} domain
- * @returns {chrome.declarativeNetRequest.Rule}
- */
-function makeRule(id, domain) {
-  return {
-    id,
-    priority: 1,
-    action: { type: "block" },
-    condition: {
-      urlFilter: domain,
-      resourceTypes: ["main_frame", "sub_frame"],
-    },
-  };
-}
-
-function isBlocked(hostname, blockedSites) {
-  return blockedSites.some((d) => {
-    if (!d || !d.includes(".")) return false;
-    return hostname === d || hostname.endsWith("." + d);
-  });
-}
-
-// ================= INIT =================
 chrome.runtime.onInstalled.addListener(async () => {
   const { blockedSites } = await chrome.storage.local.get("blockedSites");
-
   if (!blockedSites) {
     await chrome.storage.local.set({ blockedSites: DEFAULT_BLOCKED });
   }
 });
+
+//  when browser will open 
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+  const tab = await chrome.tabs.get(activeInfo.tabId);
+  handleSiteChange(tab);
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete") {
+    handleSiteChange(tab);
+  }
+});
+function onSiteVisit(site) {
+  console.log("User VISITED:", site);
+}
+
+async function onSiteLeave(site, timeSpent) {
+  const { screenTime = {} } = await chrome.storage.local.get("screenTime");
+  screenTime[site] = (screenTime[site] || 0) + timeSpent;
+  console.log(screenTime);
+  await chrome.storage.local.set({ screenTime });
+}
+
+
+// this Function will handel how much  time user will chenge the Tap
+async function handleSiteChange(tab) {
+  if (!tab.url) return;
+
+  const url = new URL(tab.url);
+  const newSite = url.hostname;
+  const now = Date.now();
+
+  console.log(currentSite, startTime);
+  if (currentSite && startTime) {
+    const timeSpent = now - startTime;
+    await onSiteLeave(currentSite, timeSpent);
+  }
+
+  //this  function will control the how much time user Will stay in the Website
+  onSiteVisit(newSite);
+
+  currentSite = newSite;
+  startTime = now;
+}
